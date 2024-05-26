787247d10b0639325f2925258ddc3b5790526ebe
justmirshod <mirshodmurodov67@gmail.com>
2024-05-26 03:20:43 -0700

    initial setup

---
+++ src/components/BasicInput.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const BasicInput = () => {
+  return <div>BasicInput</div>;
+};
+
+export default BasicInput;
---
+++ src/theme/palette.js
@@ -0,0 +1,29 @@
+export const primary = {
+  main: "#6200EE",
+};
+
+export const dark = {
+  main: "#151515",
+  grey: "rgba(21, 21, 21, 0.28)",
+};
+
+export const warning = {
+  main: "#FF4D4F",
+};
+
+export const white = {
+  main: "#FFFFFF",
+  grey: "#FEFEFE",
+};
+
+export const danger = {
+  main: "#FF0000",
+};
+
+export const yellow = {
+  main: "#FFEC43",
+};
+
+export const success = {
+  main: "#00FF29",
+};
