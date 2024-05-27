45857d767297558b15e9029ce1aaf88806a2f843
justmirshod <mirshodmurodov67@gmail.com>
2024-05-27 08:47:35 -0700

    not-found page finished

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,7 +1,52 @@
 import React from "react";
+import BackgroundContainer from "../components/BackgroundContainer";
+import FlexCenter from "../components/FlexCenter";
+import notFoundImage from "../assets/not-found.png";
+import { Box, Button, Stack } from "@mui/material";
+import { useNavigate } from "react-router-dom";
 
 const NotFound = () => {
-  return <div>NotFound</div>;
+  const navigate = useNavigate();
+
+  const handleReload = () => {
+    window.location.reload();
+  };
+
+  return (
+    <BackgroundContainer>
+      <FlexCenter>
+        <Stack
+          direction="column"
+          alignItems="center"
+          justifyContent="center"
+          gap={5}
+        >
+          <Box component="img" src={notFoundImage} />
+          <Stack
+            direction="row"
+            alignItems="center"
+            justifyContent="center"
+            gap={3}
+          >
+            <Button
+              variant="contained"
+              sx={{ textTransform: "none", width: "150px" }}
+              onClick={() => navigate("/")}
+            >
+              Go Home Page
+            </Button>
+            <Button
+              variant="outlined"
+              sx={{ textTransform: "none", width: "150px" }}
+              onClick={handleReload}
+            >
+              Reload Page
+            </Button>
+          </Stack>
+        </Stack>
+      </FlexCenter>
+    </BackgroundContainer>
+  );
 };
 
 export default NotFound;
